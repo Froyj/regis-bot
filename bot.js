33b036c6c3e41acf62bd84a12179879863f09e7f
Geoffroy Periot <geoffroy.periot@gmail.com>
2018-11-29 20:40:49 +0100

    nouveaux changements

--- bot.js
+++ bot.js
@@ -1,51 +1,73 @@
+/**
+ * A ping pong bot, whenever you send "ping", it replies "pong".
+ */
 
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-  colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-  token: auth.token,
-  autorun: true
-});
-bot.on('ready', function (evt) {
-  logger.info('Connected');
-  logger.info('Logged in as: ');
-  logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-  // Our bot needs to know if it will execute a command
-  // It will listen for messages that will start with `!`
-  if (message.substring(0, 1) == '!') {
-    var args = message.substring(1).split(' ');
-    var cmd = args[0];
+// Import the discord.js module
+const { Client, Attachment } = require('discord.js');
+const moderate = require('./moderate')
 
-    args = args.splice(1);
-    switch (cmd) {
-      // !ping
-      case 'ping':
-        bot.sendMessage({
-          to: channelID,
-          message: 'Pong!'
-        });
-      break;
-      case 'warcraft':
-        bot.sendMessage({
-          to: channelID,
-          message: 'Hey au fait! y\'a un super jeu qui sort bientôt et ca s\'appelle warcraft 3 : Reforged'
-        });
-      break;
-      case 'whoisthere':
-        bot.sendMessage({
-          to: channelID,
-          message: ''
-        });
-      // Just add any case commands if you want to..
-    }
+
+// Create an instance of a Discord client
+const client = new Client();
+const fetch = require('node-fetch');
+
+
+// async function getPlayerStats(battleTag) {
+//   const response = await fetch(`http://owapi.net/api/v3/u/${battleTag}/stats`);
+//   const stats = await response.json();
+//   return stats;
+// }
+
+async function getPlayerStats(battleTag) {
+  // GET a list of book IDs of the current user
+  return await fetch(`http://owapi.net/api/v3/u/${battleTag}/stats`)
+    .then(res => console.log(res))
+    .catch(err => {
+      return 'y\'a pas de donnée, ce gars la doit être mauvais';
+    })
+}
+
+function sendStats(battleTag) {
+  getPlayerStats(battleTag)
+    .then(res => message.channel.send(res))
+}
+
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
+  if (message.content === '!rip') {
+    // Create the attachment using Attachment
+    const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
+    // Send the attachment in the message channel
+    message.channel.send(attachment);
+  }
+  if (message.content.startsWith === '!bully') {
+    moderate.bully(message);
+  }
+  const msgArr = message.content.split(' ')
+  const cmd = msgArr[0]
+  const battleTag = msgArr[1]
+  if (cmd === '!stats' && battleTag) {
+    sendStats(battleTag)
+  } else {
+    message.channel.send("On comprend rien à ce que tu dis! (!stats BattleTag-23415)");
   }
 });
+
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login('');
+
+connection()
